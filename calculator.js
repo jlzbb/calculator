10a1c9b40fdeeec1b4c98ff57f67caa7a2ebfee7
Leonjlz <101525079+jlzbb@users.noreply.github.com>
2025-03-11 17:14:50 +0900

    created arithmetic functions

---
+++ calculator.js
@@ -0,0 +1,18 @@
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    if (b === 0) {
+        alert("Division by Zero is not allowed!");
+    }
+    return a / b;
+};
